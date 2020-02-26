a3eab2cf5562d337c750b7cf6e1e0ebabf29ee0e
Arthur57255 <39014067+Arthur57255@users.noreply.github.com>
2020-02-26 17:54:53 +0100

    Application body in progress

--- app/src/main/java/com/example/chargepoint/HomeFrag.java
+++ app/src/main/java/com/example/chargepoint/HomeFrag.java
@@ -3,11 +3,15 @@ package com.example.chargepoint;
 
 import android.os.Bundle;
 
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,12 +24,36 @@ public class HomeFrag extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button home_button = view.findViewById(R.id.btn_home);
+
+        Button map_button = view.findViewById(R.id.btn_map);
+        map_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_homeFrag_to_mapFrag);
+            }
+        });
+
+        Button rate_button = view.findViewById(R.id.btn_rates);
+        rate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_homeFrag_to_ratesFrag);
+            }
+        });
+
+        Button profile_button = view.findViewById(R.id.btn_profile);
     }
 
 }
