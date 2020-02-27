3fbf3bf7310234bde6ae7252643a970f0647a272
Art Maguire <35304676+artmaguire@users.noreply.github.com>
2020-02-27 23:04:56 +0000

    Just started profile page, working on UI elements and how to display settings on page

--- app/src/main/java/com/example/chargepoint/WelcomeFrag.java
+++ app/src/main/java/com/example/chargepoint/WelcomeFrag.java
@@ -2,12 +2,15 @@ package com.example.chargepoint;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 
 /**
@@ -28,4 +31,16 @@ public class WelcomeFrag extends Fragment {
         return inflater.inflate(R.layout.fragment_welcome, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button profile = view.findViewById(R.id.profileBtn);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_welcomeFrag2_to_profileFrag);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/chargepoint/profileFrag.java
+++ app/src/main/java/com/example/chargepoint/profileFrag.java
@@ -1,12 +1,16 @@
 package com.example.chargepoint;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +25,12 @@ public class profileFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receipt, container, false);
+        Log.v(TAG, "profile");
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
