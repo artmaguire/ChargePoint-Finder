6a4c0edc4fcced9789e5a6bf92c708beb345c77d
Corentin <corentinvas57@gmail.com>
2020-03-04 20:08:46 +0000

    Sign in done

--- app/src/main/java/com/example/chargepoint/WelcomeFrag.java
+++ app/src/main/java/com/example/chargepoint/WelcomeFrag.java
@@ -3,11 +3,15 @@ package com.example.chargepoint;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -29,3 +33,4 @@ public class WelcomeFrag extends Fragment {
     }
 
 }
+
