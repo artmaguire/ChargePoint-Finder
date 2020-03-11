6cfb491b051502525b76541e8df9c38c12b0f8c7
Art Maguire <a.elnino11@gmail.com>
2020-03-11 15:41:10 +0000

    Added UI for change email activity, no functionality

--- app/src/main/java/com/example/chargepoint/ChangeEmailActivity.java
+++ app/src/main/java/com/example/chargepoint/ChangeEmailActivity.java
@@ -1,14 +1,31 @@
 package com.example.chargepoint;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChangeEmailActivity extends AppCompatActivity {
+    Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_email);
+
+        setTitle("Change Email");
+
+        saveBtn = findViewById(R.id.saveEmailButton);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO update email address of user in the database
+                Intent intent = new Intent(ChangeEmailActivity.this, UpdateInformationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
