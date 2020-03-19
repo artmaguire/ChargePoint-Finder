46f461d9892ad97f70cbf8a08d9d4e64c281e832
Art Maguire <a.elnino11@gmail.com>
2020-03-19 21:36:00 +0000

    Updates to the UI for change email and password, needs finishing

--- app/src/main/java/com/example/chargepoint/UpdateInformationActivity.java
+++ app/src/main/java/com/example/chargepoint/UpdateInformationActivity.java
@@ -15,15 +15,15 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import com.google.android.material.textfield.TextInputEditText;
-
 public class UpdateInformationActivity extends AppCompatActivity {
     Button backBtn;
-    EditText usersName;
     CardView card;
     LinearLayout nameLayout;
+    LinearLayout emailLayout;
+    LinearLayout passwordLayout;
     TextView username;
-    TextInputEditText nameInput;
+    TextView email;
+    TextView password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,17 +49,6 @@ public class UpdateInformationActivity extends AppCompatActivity {
         card = findViewById(R.id.NameCard);
         nameLayout = findViewById(card.getChildAt(0).getId());
         username = findViewById(nameLayout.getChildAt(1).getId());
-
-//        List<String> id = new ArrayList<>();
-
-//        for (int i = 0; i < cardLayout.getChildCount(); i++){
-//            card = (CardView) cardLayout.getChildAt(i);
-//            ViewGroup viewGroup = ((ViewGroup)card.getChildAt(0));
-//            for(int j=0;j<viewGroup.getChildCount();j++){
-//                String getName = ((TextView)viewGroup.getChildAt(j)).getText().toString();
-//                id.add(getName);
-//            }
-//        }
 
         // Setting Dialog Title
         alertDialog.setTitle("Change Name");
@@ -101,8 +90,49 @@ public class UpdateInformationActivity extends AppCompatActivity {
     }
 
     public void changeEmail(View v) {
-        Intent intent = new Intent(UpdateInformationActivity.this, ChangeEmailActivity.class);
-        startActivity(intent);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(UpdateInformationActivity.this);
+
+        card = findViewById(R.id.EmailCard);
+        nameLayout = findViewById(card.getChildAt(0).getId());
+        username = findViewById(nameLayout.getChildAt(1).getId());
+
+        // Setting Dialog Title
+        alertDialog.setTitle("Change Name");
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        //TODO: Add user's name here
+//        input.setHint(R.string.);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        alertDialog.setView(input);
+
+
+        // Setting Positive "Yes" Btn
+        alertDialog.setPositiveButton("Save",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog
+                        username.setText(input.getText().toString());
+
+                        //TODO: Add floating action button
+                        Toast.makeText(getApplicationContext(),
+                                "Name updated", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+
+        // Setting Negative "NO" Btn
+        alertDialog.setNegativeButton("Back",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog
+                    }
+                });
+
+        // Showing Alert Dialog
+        alertDialog.show();
+//        Intent intent = new Intent(UpdateInformationActivity.this, ChangeNameActivity.class);
+//        startActivity(intent);
     }
 
     public void changePassword(View v) {
