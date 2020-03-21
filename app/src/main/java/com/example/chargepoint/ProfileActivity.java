fb5a5ce584f1b61387fdee38cccf46eeaf6563c8
Corentin <corentinvas57@gmail.com>
2020-03-21 21:56:12 +0100

    test push

--- app/src/main/java/com/example/chargepoint/ProfileActivity.java
+++ app/src/main/java/com/example/chargepoint/ProfileActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+//test push
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
