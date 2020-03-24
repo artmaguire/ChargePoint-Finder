e43b73eb3faba1b226263adc35f00c62a6843def
Michael Aroyehun <48759666+tobillion19@users.noreply.github.com>
2020-03-24 23:49:58 +0000

    Added Payment Functionality

--- app/src/main/java/com/example/chargepoint/PreviousReceiptsActivity.java
+++ app/src/main/java/com/example/chargepoint/PreviousReceiptsActivity.java
@@ -9,6 +9,6 @@ public class PreviousReceiptsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_previous_receipts);
+        setContentView(R.layout .activity_previous_receipts);
     }
 }
