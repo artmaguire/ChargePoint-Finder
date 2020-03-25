b7402439f097f441d720f1f8b5dbd7b491e20a33
Art Maguire <a.elnino11@gmail.com>
2020-03-25 14:33:35 +0000

    Fix Profile Preferences freezing BottomNavigationView

--- app/src/main/java/com/example/chargepoint/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/chargepoint/ui/profile/ProfileFragment.java
@@ -1,50 +1,21 @@
 package com.example.chargepoint.ui.profile;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.chargepoint.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends PreferenceFragmentCompat {
 
     private static final String TAG = "PROFILE_ACTIVITY";
     private ProfileViewModel profileViewModel;
     private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        //profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-
-        //get current user details
-        Log.d(TAG, "User Name: " + currentUser.getDisplayName());
-        Log.d(TAG, "User Email: " + currentUser.getEmail());
-        Log.d(TAG, "User ID: " + currentUser.getUid());
-
-        getActivity().getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
-
-        return root;
-    }
-
-    public static class SettingsFragment extends PreferenceFragment {
-
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            addPreferencesFromResource(R.xml.preferences);
-        }
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
