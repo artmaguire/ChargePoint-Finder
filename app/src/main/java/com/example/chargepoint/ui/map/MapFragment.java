9d1d772f3ea9c2157f9bf72012db0761b20d9142
Art Maguire <a.elnino11@gmail.com>
2020-03-26 11:04:20 +0000

    Rename dashboard files to map, and add black map icon

--- app/src/main/java/com/example/chargepoint/ui/map/MapFragment.java
+++ app/src/main/java/com/example/chargepoint/ui/map/MapFragment.java
@@ -1,4 +1,4 @@
-package com.example.chargepoint.ui.dashboard;
+package com.example.chargepoint.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.chargepoint.R;
 
-public class DashboardFragment extends Fragment {
+public class MapFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private MapViewModel mapViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mapViewModel =
+                ViewModelProviders.of(this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
