d45f5cf1d845d19439de9c8f45eee965b079288e
Art Maguire <a.elnino11@gmail.com>
2020-04-03 14:11:27 +0100

    Added functionality on Main Activity to add User to Database when sign up is complete
    Signed-off-by: artmaguire <a.elnino11@gmail.com>

--- app/src/main/java/com/example/chargepoint/activities/MainActivity.java
+++ app/src/main/java/com/example/chargepoint/activities/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.chargepoint.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,18 +15,28 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.chargepoint.R;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_REQUEST_CODE = 1234;
+    private static final String TAG = "User_db";
     List<AuthUI.IdpConfig> providers;
 
     @Override
@@ -69,9 +81,48 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Toast.makeText(this, "" + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                addUserToDb();
             } else {
                 Toast.makeText(this, "" + response.getError().getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void addUserToDb() {
+        // Check if user is already in Users table, if not add to table
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        firebaseFirestore.collection("users").document(currentFirebaseUser.getUid()).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: User already exists");
+                        } else {
+                            // Add user data to User Table
+                            Map<String, Object> userData = new HashMap<>();
+                            userData.put("uid", currentFirebaseUser.getUid());
+                            userData.put("name", currentFirebaseUser.getDisplayName());
+                            userData.put("email", currentFirebaseUser.getEmail());
+
+                            firebaseFirestore.collection("users")
+                                    .add(userData)
+                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error adding document", e);
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 }
