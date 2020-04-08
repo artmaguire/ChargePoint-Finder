8186f06a79b1666231c869ad512cf65617a0f945
Michael Aroyehun <48759666+tobillion19@users.noreply.github.com>
2020-04-08 23:44:38 +0100

    re structured the layout to the xml file of the car details page

--- app/src/main/java/com/example/chargepoint/fragments/CarDetailsFragment.java
+++ app/src/main/java/com/example/chargepoint/fragments/CarDetailsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,7 @@ import com.example.chargepoint.R;
  */
 public class CarDetailsFragment extends Fragment {
 
+    Spinner spinnerManufacturer, spinnerModel   ;
     public CarDetailsFragment() {
         // Required empty public constructor
     }
