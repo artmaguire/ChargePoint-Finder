10d20072562369e1cbefe41564d6f70278a78543
Michael Aroyehun <48759666+tobillion19@users.noreply.github.com>
2020-04-13 19:58:19 +0100

    Created DatePickerDialog for the EditText where user input the expiry date for the debit card

--- app/src/main/java/com/example/chargepoint/activities/PaymentDetailsActivity.java
+++ app/src/main/java/com/example/chargepoint/activities/PaymentDetailsActivity.java
@@ -1,14 +1,25 @@
 package com.example.chargepoint.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.example.chargepoint.R;
 import com.razorpay.Checkout;
@@ -16,13 +27,41 @@ import com.razorpay.PaymentResultListener;
 
 import org.json.JSONObject;
 
+import java.util.Calendar;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class PaymentDetailsActivity extends AppCompatActivity {
-    
+
+    DatePickerDialog datePickerDialog;
+    EditText txtMonthYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
 
+        txtMonthYear = (EditText) findViewById(R.id.txtMonthYear);
+
+        txtMonthYear.setOnClickListener((View.OnClickListener) this);
+
+        Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this,
+                AlertDialog.THEME_HOLO_DARK,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        txtMonthYear.setText((monthOfYear + 1) +"/"+ year);
+                    }
+                },mDay,mMonth,mDay);
+        datePickerDialog.show();
     }
+
+
 }
+
+
+
