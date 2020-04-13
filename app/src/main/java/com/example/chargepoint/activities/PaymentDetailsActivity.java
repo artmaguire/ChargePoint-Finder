38cf1807c1b4d168446f1bea0a90c275e5b551e4
Art Maguire <35304676+artmaguire@users.noreply.github.com>
2020-04-13 22:44:47 +0100

    Fix PaymentDetailsPage, not crashing anymore. Also remove day option from DatePickerDialog.
    Signed-off-by: artmaguire <a.elnino11@gmail.com>

--- app/src/main/java/com/example/chargepoint/activities/PaymentDetailsActivity.java
+++ app/src/main/java/com/example/chargepoint/activities/PaymentDetailsActivity.java
@@ -1,32 +1,16 @@
 package com.example.chargepoint.activities;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import com.example.chargepoint.R;
 
-
-
 import java.util.Calendar;
-
 
 public class PaymentDetailsActivity extends AppCompatActivity {
 
@@ -38,28 +22,19 @@ public class PaymentDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
 
-        txtMonthYear = (EditText) findViewById(R.id.txtMonthYear);
+        txtMonthYear = findViewById(R.id.txtMonthYear);
 
-        txtMonthYear.setOnClickListener((View.OnClickListener) this);
+        txtMonthYear.setOnClickListener(v -> {
+            Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
 
-        Calendar c = Calendar.getInstance();
-        int mYear = c.get(Calendar.YEAR);
-        int mMonth = c.get(Calendar.MONTH);
-        int mDay = c.get(Calendar.DAY_OF_MONTH);
-
-        datePickerDialog = new DatePickerDialog(this,
-                AlertDialog.THEME_HOLO_DARK,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        txtMonthYear.setText((monthOfYear + 1) +"/"+ year);
-                    }
-                },mDay,mMonth,mDay);
-        datePickerDialog.show();
+            datePickerDialog = new DatePickerDialog(this,
+                    AlertDialog.THEME_HOLO_DARK,
+                    (view, year, monthOfYear, dayOfMonth) -> txtMonthYear.setText((monthOfYear + 1) + "/" + year), mYear, mMonth, mDay);
+            datePickerDialog.getDatePicker().findViewById(getResources().getIdentifier("day", "id", "android")).setVisibility(View.GONE);
+            datePickerDialog.show();
+        });
     }
-
-
 }
-
-
-
