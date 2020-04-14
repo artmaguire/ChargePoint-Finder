fa94bdb8d6f3810758dd2eed53a8ae721eecaf3b
Michael Aroyehun <48759666+tobillion19@users.noreply.github.com>
2020-04-14 18:41:52 +0100

    Added a model package with Java classes articles, headlines and sources. they will be used for the News Fragment

---
+++ app/src/main/java/com/example/chargepoint/model/Headlines.java
@@ -0,0 +1,45 @@
+package com.example.chargepoint.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Headlines {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("totalResults")
+    @Expose
+    private String totalResults;
+
+    @SerializedName("articles")
+    @Expose
+    private List<articles> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<com.example.chargepoint.model.articles> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<com.example.chargepoint.model.articles> articles) {
+        this.articles = articles;
+    }
+}
