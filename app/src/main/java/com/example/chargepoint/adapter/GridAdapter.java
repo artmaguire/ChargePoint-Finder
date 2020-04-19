37875b2d166b85d155d784025feb677c230e9528
Arthur57255 <39014067+Arthur57255@users.noreply.github.com>
2020-04-19 14:20:03 +0200

    Add gridView for images

---
+++ app/src/main/java/com/example/chargepoint/adapter/GridAdapter.java
@@ -0,0 +1,58 @@
+package com.example.chargepoint.adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.chargepoint.R;
+
+public class GridAdapter extends BaseAdapter {
+
+    Context context;
+    private final String [] names;
+    private final int [] images;
+    View view;
+    LayoutInflater layoutInflater;
+
+    public GridAdapter(Context context, String[] names, int[] images) {
+        this.context = context;
+        this.names = names;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return names.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if(convertView == null) {
+
+            view = new View(context);
+            view = layoutInflater.inflate(R.layout.grid_item, null);
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+            TextView name = (TextView) view.findViewById(R.id.name);
+            imageView.setImageResource(images[position]);
+            name.setText(names[position]);
+        }
+        return view;
+    }
+}
